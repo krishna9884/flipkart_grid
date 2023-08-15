f1306b001e64d4bb35c5cffcb13ef4a44f81e3a7
Rohan <rshirsalkar5@gmail.com>
2023-08-15 18:03:46 +0530

    commit

--- ecom/members/views.py
+++ ecom/members/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def login_user(request):
+    if request.method=="POST":
+        email=request.POST['email']
+        password=request.POST['password']
+        user=authenticate(request,email=email,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect(' ')
+        else:
+             messages.success(request,("There was an error logging in, try again"))
+             return redirect('login')
+    else:
+        return render(request,'authenticate/login.html',{})
